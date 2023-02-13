28f26934c9b1669029693316e02f690b5b0479b1
oraclown <owen.burton13@gmail.com>
2023-02-13 13:27:05 -0600

    Add missing interface func

--- lib/tellor/interfaces/IERC20.sol
+++ lib/tellor/interfaces/IERC20.sol
@@ -5,4 +5,5 @@ interface IERC20 {
     function transfer(address _to, uint256 _amount) external returns(bool);
     function transferFrom(address _from, address _to, uint256 _amount) external returns(bool);
     function approve(address _spender, uint256 _amount) external returns(bool);
+    function balanceOf(address _owner) external view returns(uint256);
 }
