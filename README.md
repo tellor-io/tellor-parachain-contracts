39c93960aeca3e70b9a4622e0d791f72bb5183e6
oraclown <owen.burton13@gmail.com>
2023-03-07 11:58:25 -0600

    Try deposit stake invalid parachain

--- README.md
+++ README.md
@@ -10,8 +10,3 @@ See https://github.com/evilrobot-01/tellor for overview.
 - make parachain contract extendable to include the ability to set fee amounts for xcm calls when each parachain registers
 - schedule call w/ Frank to go over his side of the code
 - 
-
-
-### updates for Frank
-- `voteParachain` expects the reporters' votes to be included in `_vote` array following the users' votes
-- all gov functions take `_disputeId` instead of paraId, queryId, and timestamp
--- test/testParachainStaking.sol
+++ test/testParachainStaking.sol
@@ -7,6 +7,9 @@ import "forge-std/Vm.sol";
 import "forge-std/console.sol";
 
 import "../lib/moonbeam/precompiles/ERC20.sol";
+// Various helper methods for interfacing with the Tellor pallet on another parachain via XCM
+import "../lib/moonbeam/precompiles/XcmTransactorV2.sol";
+import "../lib/moonbeam/precompiles/XcmUtils.sol";
 
 import "../src/ParachainRegistry.sol";
 import "../src/Parachain.sol";
@@ -21,10 +24,30 @@ contract ParachainStakingTest is Test {
     ParachainRegistry public registry;
     ParachainStaking public staking;
 
+    address public bob = address(0x1111);
+
+    // XCM setup
+    // XcmTransactorV2 private constant xcmTransactor = XCM_TRANSACTOR_V2_CONTRACT;
+    // XcmUtils private constant xcmUtils = XCM_UTILS_CONTRACT;
+    uint32 public fakeParaId = 12;
+    uint8 public fakePalletInstance = 8;
+    // address public derivativeAddressOfParachain = xcmUtils.multilocationToAddress(
+    //     XcmUtils.Multilocation(1, x2(fakeParaId, fakePalletInstance)));
+
     function setUp() public {
         token = IERC20(tokenAddress);
         registry = new ParachainRegistry();
         staking = new ParachainStaking(address(registry), address(token));
+
+        // Register parachain
+        // console.log("derivativeAddressOfParachain: %s", derivativeAddressOfParachain);
+        // vm.startPrank(derivativeAddressOfParachain);
+        // registry.register(
+        //     fakeParaId, // _paraId
+        //     fakePalletInstance, // _palletInstance
+        //     100   // _stakeAmount
+        // );
+        // vm.stopPrank();
     }
 
     function testConstructor() public {
@@ -38,6 +61,19 @@ contract ParachainStakingTest is Test {
         assertEq(address(staking.governance()), address(0x1));
     }
 
+    function testDepositParachainStake() public {
+        staking.init(address(0x2));
 
+        // Try to deposit stake with incorrect parachain
+        vm.prank(bob);
+        vm.expectRevert("parachain not registered");
+        staking.depositParachainStake(
+            uint32(1234),               // _paraId
+            bytes("consumerChainAcct"), // _account
+            100                         // _amount
+        );
+        
+        // Deposit stake successfully
+    }
 
 }
