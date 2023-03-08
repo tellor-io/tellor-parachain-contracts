022d77896636a2619e7fe6aa475d4c0818254fc5
oraclown <owen.burton13@gmail.com>
2023-03-08 12:32:54 -0600

    Add mint to test token, rmv change from dep

--- test/testParachainStaking.sol
+++ test/testParachainStaking.sol
@@ -22,6 +22,9 @@ contract TestToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("TestToken", "TT", 18) {
         // _mint(msg.sender, initialSupply);
     }
+    function mint(address to, uint256 amount) external virtual {
+        _mint(to, amount);
+    }
 }
 
 contract ParachainStakingTest is Test {
